8b30bcddf5263f7ed0d6072d5c597a9a28997082
G Thimmaraju <thimmaraju418@gmail.com>
2026-08-25 15:42:38 +0530

    Locators importance

---
+++ tests/Admin/addempstatus.spec.js
@@ -0,0 +1,10 @@
+
+import { test, expect } from '@playwright/test';
+
+test("Verify admin can add employment status", async ({page}) =>{
+
+    await page.goto("/web/index.php/auth/login")
+
+    
+
+})
